99629c5fdd4e932ba941cf11a9a003cef690cedb
luis Martínez <lucas.2451@hotmail.com>
2019-04-17 17:46:27 -0500

    Implemented Accelerometer and OLED display

--- src/Snake/Snake.h
+++ src/Snake/Snake.h
@@ -6,12 +6,18 @@ class Snake
 {
 public:
     int posX, posY, maxX, maxY, minX, minY, direction;
-    
     Snake();
     Snake(int posX,int posY,int maxX,int maxY,int minX,int minY);
-
+    
+    //moves the object one position forwards direction
     void move();
+    
+    //changes direction if possible (cant go backwards)
     void turn(int direction);
+    
+    //checks if another object is at the same position
     bool crashes(Snake head);
+    
+    //changes the position randomly inside the max-min ranges
     void randPos();
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,82 @@
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
-#include "Snake.h"
+#include "Snake/Snake.h"
+#include "MPU6050.h"
 
-Snake snake[100];
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+MPU6050 sensor;
+Snake snake[10];
 Snake food;
 int points;
-void setup() {
-  // put your setup code here, to run once:
-  snake[0]=Snake(0,0,20,20,0,0);
-  food=snake[0];
+
+//Reads the accelerometer and sets direction
+int getDirection(){
+  int16_t ax,ay,az;
+  sensor.getAcceleration(&ax, &ay, &az);
+
+  if(ay<-5000)
+    return LEFT;
+  if(ay>5000)
+    return RIGHT;
+  if(ax<-5000)
+    return UP;
+  if(ax>5000)
+    return DOWN;
+  return 0;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  game();
-  endGame();
-  showHighscores();
+//prints one char at the specified coord (disabled for the moment)
+void printXY(int x, int y, int color){
+  display.drawPixel(x,y,color);
+  display.display();
 }
-
+//Resets the food's position and increases one point
+void eat(){
+  food.randPos();
+  display.drawPixel(food.posX,food.posY,WHITE);
+  points++;
+}
+// Where the player plays xD
 void game(){
+  display.clearDisplay();
   points=0;
   food.randPos();
   snake[0].randPos();
-  printXY(food.posX,food.posY,'x');
+  display.drawPixel(food.posX,food.posY,WHITE);
+
   while(true){
     snake[0].turn(getDirection());
     for (int i = points + 1; i > 0; i--)
 			snake[i] = snake[i - 1];
     snake[0].move();
     for (int i = 0; i <= points; i++)
-      printXY(snake[i].posX,snake[i].posY,'0');
-    printXY(snake[points+1].posX,snake[points+1].posY,' ');
+      display.drawPixel(snake[i].posX,snake[i].posY,WHITE);
+    display.drawPixel(snake[points+1].posX,snake[points+1].posY,BLACK);
     for(int i = 0; i < points; i++)
       if(snake[i+1].crashes(snake[0]))
         return;
     if(food.crashes(snake[0]))
       eat();
-    delay(getSpeed());
+    display.display();
   }
 }
 
-void eat(){
-  food.randPos();
-  printXY(food.posX,food.posY,'x');
-  points++;
+void loop() {
+  game();
 }
-int getDirection();
-int getSpeed();
-void printXY(int x, int y, char c);
-void showHighscores();
-void endGame();
+ 
+void setup()   {                
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  delay(100);
+  display.clearDisplay();
+  display.display();
+
+  Wire.begin();
+  sensor.initialize();   
+  snake[0]=Snake(0,0,20,20,0,0);
+  food=snake[0];
+}
